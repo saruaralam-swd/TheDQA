c315a1d1ec58a8a630335cc66b42db5992ce3180
saruaralam-swd <saruaralam2001@gmail.com>
2023-09-15 10:15:09 +0600

    top footer add

--- src/Pages/Shared/Footer/TopHeader.jsx
+++ src/Pages/Shared/Footer/TopHeader.jsx
@@ -7,56 +7,50 @@ const TopHeader = () => {
     <>
       <div className="md:grid grid-cols-3 gap-7">
         <div className="bg-[#03082D] p-5 overflow-hidden">
-          <div className="flex justify-between items-center gap-20">
-            <div className="flex items-center gap-5 ">
-              <p className="bg-orange-500 p-3 rounded-md">
-                <PhoneIcon className="w-6 h-6" />
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-4">
+              <p className="bg-orange-500 p-2 rounded-md">
+                <PhoneIcon className="w-5 h-5" />
               </p>
               <div>
-                <p>+8951452142</p>
-                <p>Give us a call</p>
+                <p className="font-medium">+8951452142</p>
+                <p className="text-xs">Give us a call</p>
               </div>
             </div>
 
-            <div>
-              <PhoneIcon className="w-10 h-10 text-[#181C3C] inline-block" />
-            </div>
+            {/* <PhoneIcon className="w-10 h-10 text-[#181C3C] inline-block" /> */}
           </div>
         </div>
 
         <div className="bg-[#03082D] p-5 overflow-hidden my-5 md:my-0">
           <div className="flex justify-between items-center gap-20">
-            <div className="flex items-center gap-5 ">
-              <p className="bg-orange-500 p-3 rounded-md">
-                <EnvelopeIcon className="w-6 h-6" />
+            <div className="flex items-center gap-4">
+              <p className="bg-orange-500 p-2 rounded-md">
+                <EnvelopeIcon className="w-5 h-5" />
               </p>
               <div>
-                <p>+8951452142</p>
-                <p>Give us a call</p>
+                <p className="font-medium">info@Theif.com</p>
+                <p className="text-xs">Drop a line</p>
               </div>
             </div>
 
-            <div>
-              <EnvelopeIcon className="w-10 h-10 text-[#181C3C] inline-block" />
-            </div>
+            {/* <EnvelopeIcon className="w-10 h-10 text-[#181C3C] inline-block" /> */}
           </div>
         </div>
 
         <div className="bg-[#03082D] p-5 overflow-hidden">
-          <div className="flex justify-between items-center gap-20">
-            <div className="flex items-center gap-5 ">
-              <p className="bg-orange-500 p-3 rounded-md">
-                <SiGooglemaps className="w-6 h-6" />
+          <div className=" gap-20">
+            <div className="flex items-center gap-4">
+              <p className="bg-orange-500 p-2 rounded-md">
+                <SiGooglemaps className="w-5 h-5" />
               </p>
               <div>
-                <p>+8951452142</p>
-                <p>Give us a call</p>
+                <p className="font-medium">58 ut Sunset road East Block</p>
+                <p className="text-xs">location</p>
               </div>
             </div>
 
-            <div>
-              <SiGooglemaps className="w-10 h-10 text-[#181C3C] inline-block" />
-            </div>
+            {/* <SiGooglemaps className="w-10 h-10 text-[#181C3C] inline-block" /> */}
           </div>
         </div>
       </div>
