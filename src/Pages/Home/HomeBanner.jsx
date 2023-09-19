ab5488e16b793bb36be48bb2767f9780d5fd9749
saruaralam-swd <saruaralam2001@gmail.com>
2023-09-20 03:15:03 +0600

    homeBanner Background add

--- src/Pages/Home/HomeBanner.jsx
+++ src/Pages/Home/HomeBanner.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import bannerImg from "../../assets/bannerImg.webp";
+import backgroundImage from "../../assets/homeBannerBg.png";
 
 const HomeBanner = () => {
+  const divStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <section className="md:flex min-h-[500px] px-10 py-8">
+    <section
+      className="md:flex min-h-[300px] px-5 py-4 md:px-10 md:py-8"
+      style={divStyle}
+    >
       <div className="md:w-1/2 order-1 flex items-center">
         <div>
-          <h2 className="text-5xl font-semibold leading-snug">
+          <h2 className="text-6xl md:text-5xl font-semibold  mb-5">
             Best Business <br /> Process OutSourcing <br /> (BPO) for Your{" "}
             <br />
-            business
+            Business
           </h2>
           <button className="bg-orange-500 text-white font-semibold px-5 py-2 rounded-md">
             Get Started
@@ -20,7 +31,7 @@ const HomeBanner = () => {
       </div>
 
       <div className="md:w-1/2 order-2 flex items-center">
-        image coming soon
+        <img src={bannerImg} alt="" />
       </div>
     </section>
   );
