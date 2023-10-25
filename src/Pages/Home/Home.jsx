dd71b0b0554fa0bca77481c0748358d69470af25
saruaralam-swd <saruaralam2001@gmail.com>
2023-10-25 12:19:39 +0600

    HomeOurMission section add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,12 +6,14 @@ import HomeTestimonial from "./HomeTestimonial";
 import HomeNewsUpdate from "./HomeNewsUpdate";
 import HomeHowItWork from "./HomeHowItWork";
 import HomeWhoChoseUs from "./HomeWhoChoseUs";
+import HomeOurMission from "./HomeOurMission";
 
 const Home = () => {
   return (
     <>
       <HomeBanner />
       <HomeAboutUs />
+      <HomeOurMission />
       <HomeServices />
       <HomeWhoChoseUs />
       <HomeHowItWork />
